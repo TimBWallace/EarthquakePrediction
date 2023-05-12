3386c11ed685a6706668e3aae39ea4e4838adcbf
TimBWallace <tbwallac@aggies.ncat.edu>
2023-05-12 20:08:01 +0900

    Cleared out main.py, and moved translated csv to dataset folder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 # Predictive analysis of earthquakes in/around Japan
 
 import pandas as pd
-from googletrans import Translator
 
-df = pd.read_csv('dataset/earthquakes_japan.csv')  # reads dataset
-print(df)
